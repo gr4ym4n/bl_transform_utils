e8f95e11f97a9bbbe46eef8b8739076ad5342262
James Snowden <jamesvsnowden@me.com>
2022-04-21 16:49:05 +0200

    update enum indexes

--- __init__.py
+++ __init__.py
@@ -11,48 +11,61 @@ LOGGER: typing.Optional[logging.Logger] = None
 LOG_ERRORS: bool = True
 
 ROTATION_MODE_ITEMS = [
-    ('AUTO'         , "Auto Euler"       , "Euler using the rotation order of the target"                                   , 'NONE', 0),
-    ('XYZ'          , "XYZ Euler"        , "Euler using the XYZ rotation order"                                             , 'NONE', 1),
-    ('XZY'          , "XZY Euler"        , "Euler using the XZY rotation order"                                             , 'NONE', 2),
-    ('YXZ'          , "YXZ Euler"        , "Euler using the YXZ rotation order"                                             , 'NONE', 3),
-    ('YZX'          , "YZX Euler"        , "Euler using the YZX rotation order"                                             , 'NONE', 4),
-    ('ZXY'          , "ZXY Euler"        , "Euler using the ZXY rotation order"                                             , 'NONE', 5),
-    ('ZYX'          , "ZYX Euler"        , "Euler using the ZYX rotation order"                                             , 'NONE', 6),
-    ('QUATERNION'   , "Quaternion"       , "Quaternion rotation"                                                            , 'NONE', 7),
-    ('SWING_TWIST_X', "Swing and X Twist", "Decompose into a swing rotation to aim the X axis, followed by twist around it" , 'NONE', 8),
-    ('SWING_TWIST_Y', "Swing and Y Twist", "Decompose into a swing rotation to aim the Y axis, followed by twist around it" , 'NONE', 9),
-    ('SWING_TWIST_Z', "Swing and Z Twist", "Decompose into a swing rotation to aim the Z axis, followed by twist around it" , 'NONE', 10),
+    ('AUTO'         , "Auto Euler"       , "Euler using the rotation order of the target"                                  ),
+    ('XYZ'          , "XYZ Euler"        , "Euler using the XYZ rotation order"                                            ),
+    ('XZY'          , "XZY Euler"        , "Euler using the XZY rotation order"                                            ),
+    ('YXZ'          , "YXZ Euler"        , "Euler using the YXZ rotation order"                                            ),
+    ('YZX'          , "YZX Euler"        , "Euler using the YZX rotation order"                                            ),
+    ('ZXY'          , "ZXY Euler"        , "Euler using the ZXY rotation order"                                            ),
+    ('ZYX'          , "ZYX Euler"        , "Euler using the ZYX rotation order"                                            ),
+    ('QUATERNION'   , "Quaternion"       , "Quaternion rotation"                                                           ),
+    ('SWING_TWIST_X', "Swing and X Twist", "Decompose into a swing rotation to aim the X axis, followed by twist around it"),
+    ('SWING_TWIST_Y', "Swing and Y Twist", "Decompose into a swing rotation to aim the Y axis, followed by twist around it"),
+    ('SWING_TWIST_Z', "Swing and Z Twist", "Decompose into a swing rotation to aim the Z axis, followed by twist around it"),
     ]
 
-ROTATION_MODE_INDEX = {
-    _item[0]: _item[4] for _item in ROTATION_MODE_ITEMS
+
+ROTATION_MODE_INDEX = [
+    _item[0] for _item in ROTATION_MODE_ITEMS
+    ]
+
+ROTATION_MODE_TABLE = {
+    _item[0]: _index for _index, _item in enumerate(ROTATION_MODE_ITEMS)
     }
 
 TRANSFORM_SPACE_ITEMS = [
-    ('WORLD_SPACE'    , "World Space"    , "Transforms include effects of parenting/restpose and constraints"    , 'NONE', 0),
-    ('TRANSFORM_SPACE', "Transform Space", "Transforms don't include parenting/restpose or constraints"          , 'NONE', 1),
-    ('LOCAL_SPACE'    , "Local Space"    , "Transforms include effects of constraints but not parenting/restpose", 'NONE', 2),
+    ('WORLD_SPACE'    , "World Space"    , "Transforms include effects of parenting/restpose and constraints"    ),
+    ('TRANSFORM_SPACE', "Transform Space", "Transforms don't include parenting/restpose or constraints"          ),
+    ('LOCAL_SPACE'    , "Local Space"    , "Transforms include effects of constraints but not parenting/restpose"),
     ]
 
-TRANSFORM_SPACE_INDEX = {
-    _item[0]: _item[4] for _item in TRANSFORM_SPACE_ITEMS
+TRANSFORM_SPACE_INDEX = [
+    _item[0] for _item in TRANSFORM_SPACE_ITEMS
+    ]
+
+TRANSFORM_SPACE_TABLE = {
+    _item[0]: _index for _index, _item in enumerate(TRANSFORM_SPACE_ITEMS)
     }
 
 TRANSFORM_TYPE_ITEMS = [
-    ('LOC_X'  , "X Location", "", 'NONE', 0),
-    ('LOC_Y'  , "Y Location", "", 'NONE', 1),
-    ('LOC_Z'  , "Z Location", "", 'NONE', 2),
-    ('ROT_W'  , "W Rotation", "", 'NONE', 3),
-    ('ROT_X'  , "X Rotation", "", 'NONE', 4),
-    ('ROT_Y'  , "Y Rotation", "", 'NONE', 5),
-    ('ROT_Z'  , "Z Rotation", "", 'NONE', 6),
-    ('SCALE_X', "X Scale"   , "", 'NONE', 7),
-    ('SCALE_Y', "Y Scale"   , "", 'NONE', 8),
-    ('SCALE_Z', "Z Scale"   , "", 'NONE', 9),
+    ('LOC_X'  , "X Location", ""),
+    ('LOC_Y'  , "Y Location", ""),
+    ('LOC_Z'  , "Z Location", ""),
+    ('ROT_W'  , "W Rotation", ""),
+    ('ROT_X'  , "X Rotation", ""),
+    ('ROT_Y'  , "Y Rotation", ""),
+    ('ROT_Z'  , "Z Rotation", ""),
+    ('SCALE_X', "X Scale"   , ""),
+    ('SCALE_Y', "Y Scale"   , ""),
+    ('SCALE_Z', "Z Scale"   , ""),
     ]
 
-TRANSFORM_TYPE_INDEX = {
-    _item[0]: _item[4] for _item in TRANSFORM_TYPE_ITEMS
+TRANSFORM_TYPE_INDEX = [
+    _item[0] for _item in TRANSFORM_TYPE_ITEMS
+    ]
+
+TRANSFORM_TYPE_TABLE = {
+    _item[0]: _index for _index, _item in enumerate(TRANSFORM_TYPE_ITEMS)
     }
 
 def transform_target(object: typing.Optional[bpy.types.Object], bone_target: typing.Optional[str]="") -> typing.Optional[typing.Union[bpy.types.Object, bpy.types.PoseBone]]:
@@ -78,10 +91,10 @@ def transform_matrix(target: typing.Optional[typing.Union[bpy.types.ID, bpy.type
             
             raise TypeError(message)
 
-        if transform_space not in TRANSFORM_SPACE_INDEX:
+        if transform_space not in TRANSFORM_SPACE_TABLE:
             message = (f'transform_matrix(target, transform_space): '
                        f'transform_space {transform_space} '
-                       f'not found in {tuple(TRANSFORM_SPACE_INDEX)}.')
+                       f'not found in {tuple(TRANSFORM_SPACE_TABLE)}.')
 
             if LOG_ERRORS and isinstance(LOGGER, logging.Logger):
                 LOGGER.error(message)
@@ -124,18 +137,18 @@ def transform_matrix_element(matrix: mathutils.Matrix, transform_type: str, rota
 
             raise ValueError(message)
 
-        if transform_type not in TRANSFORM_TYPE_INDEX:
+        if transform_type not in TRANSFORM_TYPE_TABLE:
             message = (f'transform_matrix_element(matrix, transform_type, rotation_mode="AUTO", driver=False): '
-                       f'transform_type {transform_type} not found in {tuple(TRANSFORM_TYPE_INDEX)}.')
+                       f'transform_type {transform_type} not found in {tuple(TRANSFORM_TYPE_TABLE)}.')
 
             if LOG_ERRORS and isinstance(LOGGER, logging.Logger):
                 LOGGER.error(message)
 
             raise ValueError(message)
 
-        if rotation_mode not in ROTATION_MODE_INDEX:
+        if rotation_mode not in ROTATION_MODE_TABLE:
             message = (f'transform_matrix_element(matrix, transform_type, rotation_mode="AUTO", driver=False): '
-                       f'rotation_mode {rotation_mode} not found in {tuple(ROTATION_MODE_INDEX)}.')
+                       f'rotation_mode {rotation_mode} not found in {tuple(ROTATION_MODE_TABLE)}.')
 
             if LOG_ERRORS and isinstance(LOGGER, logging.Logger):
                 LOGGER.error(message)
